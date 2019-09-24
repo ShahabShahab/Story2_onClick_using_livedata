babc799fefadcc70c6f68a95040d00f83500a426
shahabshahab <shahabgolpaiegan@gmail.com>
2019-09-24 18:45:32 +0330

    migrated to androidx

--- app/src/main/java/ir/sample/story2_callback_using_livedata/MainActivity.kt
+++ app/src/main/java/ir/sample/story2_callback_using_livedata/MainActivity.kt
@@ -1,7 +1,7 @@
 package ir.sample.story2_callback_using_livedata
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
